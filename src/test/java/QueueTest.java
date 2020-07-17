d6f2d46319d41e71084b12e1a422032a1acaed88
chy <chy@git.com>
2020-07-17 11:05:02 +0800

    队列性能测试

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -1,3 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Random;
+
 /**
  * 自定义队列测试
  *
@@ -6,28 +10,27 @@
  */
 public class QueueTest {
 
+    private static double testQueue(Queue<Integer> queue, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
-
-            if (i % 3 == 2) {
-                queue.dequeue();
-                System.out.println(queue);
-            }
-        }
-
-        Queue<Integer> queue2 = new LoopQueue<>();
-        for (int i = 0; i < 10; i++) {
-            queue2.enqueue(i);
-            System.out.println(queue2);
-
-            if (i % 3 == 2) {
-                queue2.dequeue();
-                System.out.println(queue2);
-            }
-        }
+        int opCount = 100000;
+        double time1 = testQueue(new ArrayQueue<>(), opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+        double time2 = testQueue(new LoopQueue<>(), opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
     }
 
 }
