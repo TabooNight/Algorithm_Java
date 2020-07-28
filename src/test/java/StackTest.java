5c3bcb1238b11d414e7763ae89eecc8506df0c3d
chy <chy@git.com>
2020-07-28 15:25:28 +0800

    使用链表实现栈

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 自定义栈测试
  *
@@ -6,15 +8,29 @@
  */
 public class StackTest {
 
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
-        }
+    private static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
 
-        stack.pop();
-        System.out.println(stack);
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            stack.pop();
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+
+        Stack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
+
+        Stack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
     }
 
 }
